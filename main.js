5feafca7b667cff09f81a110c67ee3d8608a0667
Manuel Valls Fernández <manolo@vlrz.es>
2015-03-06 12:30:07 +0100

    Now unique counter caps at 1e15

--- main.js
+++ main.js
@@ -104,9 +104,11 @@ module.exports.string = function(n,base,useDate){
 	return str;
 };
 
-var counter = 0;
+var counter = -1;
 module.exports.unique = function(){
-  return getRandBase(62,counter++) + '-' + getRandBase(62,Date.now() - dref) + '-' + module.exports.string(5,62);
+  counter = (counter + 1)%1e15;
+  
+  return getRandBase(62,counter + '-' + getRandBase(62,Date.now() - dref) + '-' + module.exports.string(5,62);
 };
 
 if(global.Symbol){
